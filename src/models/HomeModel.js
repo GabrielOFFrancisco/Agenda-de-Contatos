b85551305a729cc47f95e87a035f001155fbe4cc
Gabriel <gabriel.offrancisco@gmail.com>
2023-10-17 18:03:44 -0300

    Adicionando cadastro

--- src/models/HomeModel.js
+++ src/models/HomeModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const HomeSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  descricao: String
+  email: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
 const HomeModel = mongoose.model('Home', HomeSchema);
